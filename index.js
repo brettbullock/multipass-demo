cb24a1e908b1b7c8ef059755965934a9197bb27f
brettbullock <brett.bullock@shopify.com>
2019-11-12 14:47:16 -0800

    prep for deploy

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
 
-
 //use config module to get the privatekey, if no private key set, end the application
-if (!config.get("myprivatekey")) {
+if (!(config.get("myprivatekey") || process.env.MY_PRIVATE_KEY)) {
   console.error("FATAL ERROR: myprivatekey is not defined.");
   process.exit(1);
 }
