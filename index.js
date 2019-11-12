35b6118e29c71a0029ac4d0d4205c45265f6423d
brettbullock <brett.bullock@shopify.com>
2019-11-11 21:15:10 -0800

    fixed untracked files

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(cors())
 //use users route for api/user
 app.use("/api/users", usersRoute);
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
